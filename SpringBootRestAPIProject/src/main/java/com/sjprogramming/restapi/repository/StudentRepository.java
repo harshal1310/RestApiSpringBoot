47cf062c5ac9b1da9318a67a338c31c8b5126b37
harshal1310 <59737009+harshal1310@users.noreply.github.com>
2024-08-17 00:32:30 +0530

    Delete SpringBootRestAPIProject/src/main/java/com/sjprogramming/restapi/repository/StudentRepository.java
    DELETE Unncessary files

--- SpringBootRestAPIProject/src/main/java/com/sjprogramming/restapi/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sjprogramming.restapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sjprogramming.restapi.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Integer>{
-
-}
